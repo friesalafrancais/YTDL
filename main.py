fafcb63a5daf51cd435d3851d09a81da20e1fc1c
friesalafrancais <115602464+friesalafrancais@users.noreply.github.com>
2023-06-10 09:58:58 -0700

    Update main.py

--- main.py
+++ main.py
@@ -10,14 +10,8 @@ main_fs = ('Comic Sans MS', 12)
 # PySimpleGUI theme
 sg.theme('DarkBlue17')
 
-# Creates the main window layout with some basic device info
-mainWindowlayout = [[sg.Text("Enter URL:", font=('Comic Sans MS', 14))],
-                    [sg.Input(key="-url-")],
-                    [sg.Button('Query Video', font=main_fs), sg.Exit(font=main_fs)]]
 
-MainWindow = sg.Window('YTDL', mainWindowlayout, icon=r'icon.ico', grab_anywhere=True)
-
-
+# Function that creates the main window and its layout
 def main_window():
     layout = [[sg.Text("Enter URL:", font=('Comic Sans MS', 14))],
                     [sg.Input(key="-url-")],
@@ -25,10 +19,18 @@ def main_window():
 
     return sg.Window('YTDL', layout, icon=r'icon.ico', grab_anywhere=True)
 
+
+# Calls the 'main_window' function
+MainWindow = main_window()
+
+
+# Used to round up the number from PyTube.length
 def round_up(n, decimals=0):
     multiplier = 10 ** decimals
     return math.ceil(n * multiplier) / multiplier
 
+
+# Main event loop
 while True:
     event, values = MainWindow.Read()
 
@@ -36,6 +38,7 @@ while True:
         MainWindow.close()
         break
 
+    # If the user enters a URL and selects 'Query Video' the main window is closed and the 'videoqueryWindow' is opened
     elif event == 'Query Video':
         url = values["-url-"]
 
@@ -45,11 +48,12 @@ while True:
 
         video_length = (my_video.length/60)
 
-        videoqueryLayout = [[sg.Text("Title: " + my_video.title, font=main_f)],
+        videoqueryLayout = [[sg.Text("Title: ", font=main_f), sg.Text(my_video.title, font=main_f)],
                             [sg.Text("Length: ", font=main_f), sg.Text(round_up(video_length, 2), font=main_f), sg.Text("Minutes", font=main_f)],
-                            [sg.Text("Author: " + my_video.author, font=main_f)],
-                            [sg.Text("Download location:")],
-                            [sg.Input(key="-location-"), sg.FolderBrowse()],
+                            [sg.Text("Author: ", font=main_f), sg.Text(my_video.author, font=main_f)],
+                            [sg.Text("")],
+                            [sg.Text("Download location:", font=main_f)],
+                            [sg.Input(key="-location-"), sg.FolderBrowse(font=main_fs)],
                         [sg.Button('Download', font=main_fs, button_color=("white", "darkgreen")), sg.Exit(font=main_fs)]]
 
         videoqueryWindow = sg.Window('YTDL', videoqueryLayout, icon=r'icon.ico', grab_anywhere=True)
@@ -60,23 +64,28 @@ while True:
 
         while keepRunning is True:
 
+            # If the user selects 'Exit' the 'videoqueryWindow' is closed and the 'MainWindow' is opened.
             if event in (None, 'Exit'):
                 videoqueryWindow.close()
                 MainWindow = main_window()
                 break
 
+            # If the user selects 'Download' the video will download to the desired folder
+            # If no download location is selected, defaults to users 'Downloads' folder
             if event == "Download":
                 downloadLocation = values2["-location-"]
 
                 print('Downloading video...')
                 if not values2["-location-"]:
-                    print("User did not specify a download location, defaulting to users Downloads folder!")
+                    print("Download location not specified, defaulting to users 'Downloads' folder!")
                     downloadLocation = str(Path.home() / "Downloads")
                 video = my_video.streams.get_highest_resolution()
                 video.download(downloadLocation)
 
                 print("Successfully downloaded!")
 
+                # Setting 'keepRunning' to 'False' ends the loop
+                # This closes 'videoqueryWindow' and opens 'MainWindow'
                 keepRunning = False
 
         videoqueryWindow.Close()
